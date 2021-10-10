1142eca37551138459a57be4831b4b0de2ff0890
freaklucas <lucas.oliveira.freak@gmail.com>
2021-10-10 01:56:43 -0300

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex flex-center">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
