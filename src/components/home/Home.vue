4ad6bb091d02f03b448988589aebff177bdf6ab2
freaklucas <lucas.oliveira.freak@gmail.com>
2021-10-11 12:35:38 -0300

    add navbar

---
+++ src/components/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+      <Header />
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/template/Header.vue'
+
+export default {
+    components: {
+        Header,
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/template/Header.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="flex">
+    <header
+      class="
+        fixed
+        items-center
+        justify-between
+        flex-wrap
+        p-4
+        w-full
+        text-gray-100
+        bg-gray-900
+        font-mono
+        shadow
+      "
+    >
+      <div
+        class="
+          container
+          mx-auto
+          flex flex-wrap
+          p-4
+          flex-col
+          md:flex-row
+          items-center
+        "
+      >
+        <nav
+          class="
+            flex
+            lg:w-full
+            flex-wrap
+            items-center
+            text-base
+            md:ml-auto
+            inline-flex
+            space-x-4
+            sm:w-full
+          "
+        >
+          <a
+            class="
+              flex-1
+              hover:text-purple-300
+              cursor-pointer
+              border-b border-transparent
+              hover:border-indigno-600
+            "
+          >
+            Lucas />
+          </a>
+          <a
+            class="
+              flex-2
+              inline-flex
+              justify-end
+              ml-2
+              mr-4
+              hover:text-purple-300
+              cursor-pointer
+              border-b border-transparent
+              hover:border-indigno-600
+            "
+          >
+            Quem sou?
+          </a>
+          <a
+            class="
+              flex-2
+              inline-flex
+              justify-end
+              ml-2
+              hover:text-purple-300
+              cursor-pointer
+              border-b border-transparent
+              hover:border-indigno-600
+            "
+          >
+            Contato
+          </a>
+          <a
+            class="
+              flex-3
+              inline-flexjustify-end
+              ml-4
+              lg:ml-0
+              hover:text-purple-300
+              cursor-pointer
+              border-b border-transparent
+              hover:border-indigno-600
+            "
+          >
+            Projetos
+          </a>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
